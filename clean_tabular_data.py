1fd4dedac59af3448fa0474bc7f95a35144ebbc0
Abubakar Gangat <abu_gangat@outlook.com>
2023-08-02 23:16:28 +0100

    updated cleaning code

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def input_data(file_path: str='products.csv', line_term: str=',') -> pd.DataFrame: # This function imports data from a .csv file and returns a pandas dataframe. It also deletes all incomplete rows.
     products_df = pd.read_csv(file_path, line_term) #utilise local parameters for conciseness and clarity. Takes in the csv as a pandas dataframe
-    products_df = products_df.dropna() # deletes rows with at least one item missing
+    products_df = products_df.dropna() # deletes rows with at least one item missing.
     return products_df
 
 def clean_price_data(price_data: pd.Series) -> pd.Series: # This function takes pandas series with prices. It returns all values as numerical (float) values by removing symbols and commas.
