2eeba94897ebc4f47c5990faa5c09c849cde6c4a
Abubakar Gangat <abu_gangat@outlook.com>
2023-08-02 23:11:54 +0100

    updated cleaning code

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -5,7 +5,7 @@ def input_data(file_path: str='products.csv', line_term: str=',') -> pd.DataFram
     products_df = products_df.dropna() # deletes rows with at least one item missing
     return products_df
 
-def clean_price_column(price_data: pd.Series) -> pd.Series: # This function takes pandas series with prices. It returns all values as numerical (float) values by removing symbols and commas.
+def clean_price_data(price_data: pd.Series) -> pd.Series: # This function takes pandas series with prices. It returns all values as numerical (float) values by removing symbols and commas.
     products_df = input_data() # Takes the first function to load the dataset as a whole.
     price_data = products_df['price'] # from the dataset, narrow down to the price data.
 
@@ -16,4 +16,4 @@ def clean_price_column(price_data: pd.Series) -> pd.Series: # This function take
     return price_data 
 
 if __name__ == "__main__":
-    clean_price_column(pd.Series)
+    clean_price_data(pd.Series)
