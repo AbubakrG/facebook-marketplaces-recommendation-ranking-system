a015e688eafaa3da341342c57874ce422da3e5ea
Abubakar Gangat <abu_gangat@outlook.com>
2023-08-03 12:28:33 +0100

    updated and fixed errors

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -1,19 +1,41 @@
+import csv
 import pandas as pd
+import chardet as cd
 
-def input_data(file_path: str='products.csv', line_term: str=',') -> pd.DataFrame: # This function imports data from a .csv file and returns a pandas dataframe. It also deletes all incomplete rows.
-    products_df = pd.read_csv(file_path, line_term) #utilise local parameters for conciseness and clarity. Takes in the csv as a pandas dataframe
-    products_df = products_df.dropna() # deletes rows with at least one item missing.
-    return products_df
+class CleanData:
 
-def clean_price_data(price_data: pd.Series) -> pd.Series: # This function takes pandas series with prices. It returns all values as numerical (float) values by removing symbols and commas.
-    products_df = input_data() # Takes the first function to load the dataset as a whole.
-    price_data = products_df['price'] # from the dataset, narrow down to the price data.
+    def _init_(self,name) -> None:
+        self.name = name
+        self.df = self.input_data(name)
 
-    price_data = price_data.str.strip('£') # Removes pound symbol.
-    price_data = price_data.replace(',','', regex=True) # Removes commas.
-    price_data = price_data.astype('float64') # Convert to float.
+    def clean_price_data(price_column: pd.Series) -> pd.Series:
+        clean_column = price_column.replace(to_replace='[^0-9.]', value='', regex=True)
+        float_column = pd.to_numeric(clean_column)
+        return float_column
 
-    return price_data 
+    def input_data(filepath: str, lineterminator: str = ",") -> pd.DataFrame:
+        df = pd.read_csv(filepath, lineterminator=lineterminator)
+        df.rename(columns={'create_time\r':'create_time'}, inplace=True)
+        return df
+
+    def load_and_clean(file_path, lineterminator):
+        tabular_data = CleanData.input_data(file_path, lineterminator)
+        tabular_data['price'] = CleanData.clean_price_data(tabular_data['price'])
+        tabular_data.rename(columns = {'id':'product_id'}, inplace = True)
+
+        return tabular_data
+
+    # products_df = input_data() # Takes the first function to load the dataset as a whole.
+    # price_data = products_df['price'] # from the dataset, narrow down to the price data.
+
+    # price_data = price_data.str.strip('£') #
+    # price_data = price_data.replace(',','', regex=True) 
+    # price_data = price_data.astype('float64') # Convert to float.
+
+    # return price_data 
+
 
 if __name__ == "__main__":
-    clean_price_data(pd.Series)
+    file_path = "products.csv"
+    lineterminator = "\n"
+    CleanData.load_and_clean(file_path, lineterminator)  
